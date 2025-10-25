deac09161917be588ec84ea8c1d8d3d42b38344b
tzaf <homedigsoftware@gmail.com>
2025-10-26 00:26:20 +0300

    fixing the macro function i get an error on the type fo the date becuse of the recursive date_seriesall fixed with copilot

--- sakila_DBT/macros/generate_date_dimension.sql
+++ sakila_DBT/macros/generate_date_dimension.sql
@@ -1,10 +1,11 @@
-{% macro generate_date_dimension(start_date, week_start_day='monday') %}
+
+{% macro generate_date_dimension(start_date, week_start_day='sunday') %}
     with recursive date_series as (
         select 
             cast('{{ start_date }}' as date) as date_day
         union all
         select 
-            date_day + interval '1 day'
+            cast(date_day + interval '1 day' as date)
         from date_series
         where date_day + interval '1 day' <= current_date
     )
@@ -12,32 +13,34 @@
         date_day,
         extract(year from date_day) as year,
         extract(month from date_day) as month_number,
-        to_char(date_day, 'Month') as month_name,
+        trim(to_char(date_day, 'Month')) as month_name,
         extract(day from date_day) as day_number,
         extract(dow from date_day) as day_of_week_number,
-        to_char(date_day, 'Day') as day_name,
+        trim(to_char(date_day, 'Day')) as day_name,
         extract(quarter from date_day) as quarter,
-        
+
+        -- תחילת השבוע לפי יום ראשון
         case 
             when lower('{{ week_start_day }}') = 'sunday' 
-                then date_day - ((extract(dow from date_day) + 6) % 7) * interval '1 day'
+                then date_day - extract(dow from date_day) * interval '1 day'
             else 
                 date_day - (extract(dow from date_day) - 1) * interval '1 day'
         end as week_start_date,
-        
+
+        -- סוף השבוע לפי יום ראשון (שבת)
         case 
             when lower('{{ week_start_day }}') = 'sunday'
-                then date_day + (7 - ((extract(dow from date_day) + 6) % 7) - 1) * interval '1 day'
+                then date_day + (6 - extract(dow from date_day)) * interval '1 day'
             else 
                 date_day + (7 - (extract(dow from date_day) - 1) - 1) * interval '1 day'
         end as week_end_date,
 
+        -- שישי ושבת כסוף שבוע
         case 
-            when extract(dow from date_day) in (0,6) then true
+            when extract(dow from date_day) in (5, 6) then true
             else false
         end as is_weekend,
 
-        -- year-week (useful for grouping)
         to_char(date_day, 'IYYY-IW') as year_week
 
     from date_series
--- sakila_DBT/models/example/Dimensions/dim_date_macro.sql
+++ sakila_DBT/models/example/Dimensions/dim_date_macro.sql
@@ -2,7 +2,7 @@
 
 {{ 
     generate_date_dimension(
-        start_date='01-01-2020',
+        start_date='01-01-2010',
         week_start_day='sunday'
     ) 
 }}
